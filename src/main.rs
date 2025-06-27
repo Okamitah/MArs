8c92e21c80bdc21d814350f77b014dc79af7ebf6
Okamitah <179641048+Okamitah@users.noreply.github.com>
2025-06-27 17:15:53 +0100

    Added the new, zeroes, ones tensors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
-    let tensor = Tensor {
-        data: TensorData::F32(vec![1.0, 1.1, 1.3]),
-        shape: vec![3],
-        strides: None,
-        dtype: DType::F32,
-    };
+    let data = vec![1.5, 7.2, 5.0, 2.6];
+    let shape = vec![2,2];
+    //let strides = None;
+    let dtype = DType::F32;
+    let tensor = Tensor::new(data, shape.clone(), dtype.clone());
+    let zeroes = Tensor::zeroes(shape.clone(), dtype.clone());
+    let ones = Tensor::ones(shape.clone(), dtype.clone());
     println!("{:?}", tensor);
+    println!("{:?}", zeroes);
+    println!("{:?}", ones);
 
 }
 
@@ -15,7 +18,7 @@ enum TensorData {
     I32(Vec<i32>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum DType {
     F32,
     I32,
@@ -27,4 +30,63 @@ struct Tensor {
     shape: Vec<usize>,
     strides: Option<Vec<usize>>,
     dtype: DType,
+}
+
+impl Tensor {
+    pub fn new(data: Vec<f32>, shape: Vec<usize>, dtype: DType) -> Self {
+        Self {
+            data: TensorData::F32(data),
+            shape,
+            strides: None,
+            dtype,
+        }
+    }
+
+    pub fn zeroes(shape: Vec<usize>, dtype: DType) -> Self {
+        let size = shape.iter().product();
+        match dtype {
+            DType::F32 => {
+                let data = vec![0.0; size];
+                Self {
+                    data: TensorData::F32(data),
+                    shape,
+                    strides: None,
+                    dtype: DType::F32,
+                }
+            }
+            DType::I32 => {
+                let data = vec![0; size];
+                Self {
+                    data: TensorData::I32(data),
+                    shape,
+                    strides: None,
+                    dtype: DType::I32,
+                }
+            }
+        }
+    }
+
+    pub fn ones(shape: Vec<usize>, dtype: DType) -> Self {
+        let size = shape.iter().product();
+        match dtype {
+            DType::F32 => {
+                let data = vec![1.0; size];
+                Self {
+                    data: TensorData::F32(data),
+                    shape,
+                    strides: None,
+                    dtype: DType::F32,
+                }
+            }
+            DType::I32 => {
+                let data = vec![1; size];
+                Self {
+                    data: TensorData::I32(data),
+                    shape,
+                    strides: None,
+                    dtype: DType::I32,
+                }
+            }
+        }
+    }
 }
