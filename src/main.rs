fb6576c579354c5ba2f0b54f7e2436035600558b
Okamitah <179641048+Okamitah@users.noreply.github.com>
2025-06-27 17:44:40 +0100

    Made the simple tensor arithmetic operations (add, sub, mul, div)

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,20 @@ fn main() {
     let tensor = Tensor::new(data, shape.clone(), dtype.clone());
     let zeroes = Tensor::zeroes(shape.clone(), dtype.clone());
     let ones = Tensor::ones(shape.clone(), dtype.clone());
+
     println!("{:?}", tensor);
     println!("{:?}", zeroes);
     println!("{:?}", ones);
+
+    let somme1 = tensor.add(&ones);
+    let somme2 = tensor.add(&zeroes);
+    let mult1 = tensor.mul(&ones);
+    let mult2 = tensor.mul(&zeroes);
+
+    println!("Somme 1: {:?}", somme1);
+    println!("Somme 2: {:?}", somme2);
+    println!("Mult 1: {:?}", mult1);
+    println!("Mult 2: {:?}", mult2);
 
 }
 
@@ -22,6 +33,12 @@ enum TensorData {
 enum DType {
     F32,
     I32,
+}
+
+#[derive(Debug)]
+enum TensorError {
+    TypeMismatch,
+    ShapeMismatch,
 }
 
 #[derive(Debug)]
@@ -89,4 +106,118 @@ impl Tensor {
             }
         }
     }
+
+    pub fn add(&self, tensor: &Self) -> Result<Self, TensorError> {
+        if self.shape != tensor.shape {
+            return Err(TensorError::ShapeMismatch);
+        }
+
+        match (&self.data, &tensor.data) {
+            (TensorData::F32(a), TensorData::F32(b)) => {
+                let result: Vec<f32> = a.iter().zip(b).map(|(&x, &y)| x+y).collect();
+                Ok(Tensor {
+                    data: TensorData::F32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::F32
+                })
+            },
+            (TensorData::I32(a), TensorData::I32(b)) => {
+                let result: Vec<i32> = a.iter().zip(b).map(|(&x, &y)| x+y).collect();
+                Ok(Tensor {
+                    data: TensorData::I32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::I32
+                })
+            },
+            _ => Err(TensorError::TypeMismatch)
+        }
+    }
+
+    pub fn mul(&self, tensor: &Self) -> Result<Self, TensorError> {
+        if self.shape != tensor.shape {
+            return Err(TensorError::ShapeMismatch);
+        }
+
+        match (&self.data, &tensor.data) {
+            (TensorData::F32(a), TensorData::F32(b)) => {
+                let result: Vec<f32> = a.iter().zip(b).map(|(&x, &y)| x*y).collect();
+                Ok(Tensor {
+                    data: TensorData::F32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::F32
+                })
+            },
+            (TensorData::I32(a), TensorData::I32(b)) => {
+                let result: Vec<i32> = a.iter().zip(b).map(|(&x, &y)| x*y).collect();
+                Ok(Tensor {
+                    data: TensorData::I32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::I32
+                })
+            },
+            _ => Err(TensorError::TypeMismatch)
+        }
+    }
+
+    pub fn sub(&self, tensor: &Self) -> Result<Self, TensorError> {
+        if self.shape != tensor.shape {
+            return Err(TensorError::ShapeMismatch);
+        }
+
+        match (&self.data, &tensor.data) {
+            (TensorData::F32(a), TensorData::F32(b)) => {
+                let result: Vec<f32> = a.iter().zip(b).map(|(&x, &y)| x-y).collect();
+                Ok(Tensor {
+                    data: TensorData::F32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::F32
+                })
+            },
+            (TensorData::I32(a), TensorData::I32(b)) => {
+                let result: Vec<i32> = a.iter().zip(b).map(|(&x, &y)| x-y).collect();
+                Ok(Tensor {
+                    data: TensorData::I32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::I32
+                })
+            },
+            _ => Err(TensorError::TypeMismatch)
+        }
+    }
+
+
+    pub fn div(&self, tensor: &Self) -> Result<Self, TensorError> {
+        if self.shape != tensor.shape {
+            return Err(TensorError::ShapeMismatch);
+        }
+
+        match (&self.data, &tensor.data) {
+            (TensorData::F32(a), TensorData::F32(b)) => {
+                let result: Vec<f32> = a.iter().zip(b).map(|(&x, &y)| x/y).collect();
+                Ok(Tensor {
+                    data: TensorData::F32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::F32
+                })
+            },
+            (TensorData::I32(a), TensorData::I32(b)) => {
+                let result: Vec<i32> = a.iter().zip(b).map(|(&x, &y)| x/y).collect();
+                Ok(Tensor {
+                    data: TensorData::I32(result),
+                    shape: self.shape.clone(),
+                    strides: None,
+                    dtype: DType::I32
+                })
+            },
+            _ => Err(TensorError::TypeMismatch)
+        }
+    }
+
 }
