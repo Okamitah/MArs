649530bb4011c1645a555753ded899e5c17b631c
Okamitah <179641048+Okamitah@users.noreply.github.com>
2025-07-02 12:14:06 +0100

    Added 2 classification loss functions
    Added cross entropy and hinge

--- src/main.rs
+++ src/main.rs
@@ -247,6 +247,8 @@ impl Tensor {
     }
 }
 
+// Regression loss functions
+
 fn mae(pred: &Tensor, target: &Tensor) -> Result<f64, TensorError> {
     if pred.shape != target.shape {
         return Err(TensorError::TypeMismatch);
@@ -390,4 +392,67 @@ fn smae(pred: &Tensor, target: &Tensor, delta: f64) -> Result<f64, TensorError> 
         },
         _ => return Err(TensorError::TypeMismatch),
     }
+}
+
+// Classification loss functions
+
+fn cross_entropy(pred: &Tensor, target: &Tensor) -> Result<f64, TensorError> {
+    
+    if pred.shape != target.shape {
+        return Err(TensorError::TypeMismatch);
+    }
+
+    let mut error = 0.0;
+    let size = pred.shape.iter().product();
+
+    match (&pred.data, &target.data) {
+        (TensorData::F64(a), TensorData::F64(b)) => {
+            for i in 0..size {
+                let err = - (b[i] * a[i].log10() + (1.0 - b[i]) * (1.0 - a[i]).log10());
+                error += err;
+            }
+            Ok(error/size as f64)
+        },
+        (TensorData::I32(a), TensorData::I32(b)) => {
+            for i in 0..size {
+                let err = - (b[i] as f64 * (a[i] as f64).log10() + (1.0 - b[i] as f64) * (1.0 - a[i] as f64).log10());
+                error += err;
+            }
+            Ok(error/size as f64)
+        },
+        _ => return Err(TensorError::TypeMismatch),
+    }
+}
+
+fn hinge(pred: &Tensor, target: &Tensor) -> Result<f64, TensorError> {
+    
+    if pred.shape != target.shape {
+        return Err(TensorError::TypeMismatch);
+    }
+
+    let mut error = 0.0;
+    let size = pred.shape.iter().product();
+
+    match (&pred.data, &target.data) {
+        (TensorData::F64(a), TensorData::F64(b)) => {
+            for i in 0..size {
+                let err = max(0.0, 1.0 - b[i] * a[i]);
+                error += err;
+            }
+            Ok(error/size as f64)
+        },
+        (TensorData::I32(a), TensorData::I32(b)) => {
+            for i in 0..size {
+                let err = max(0.0, 1.0 - b[i] as f64 * a[i] as f64);
+                error += err;
+            }
+            Ok(error/size as f64)
+        },
+        _ => return Err(TensorError::TypeMismatch),
+    }
+}
+
+fn max(num1: f64, num2: f64) -> f64 {
+    if num1 > num2 { return num1; }
+    num2
 }
